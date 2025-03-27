daaebedbc59ba2bbbbf28921d96fe88d29e14d08
MinHyeok Choi <160010293+minhyeok1232@users.noreply.github.com>
2025-03-27 20:48:14 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -1,29 +1,38 @@
 #include <iostream>
 #include <stack>
 using namespace std;
-int main() {
-    int N;
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        string s;
-        stack<char> st;
-        cin >> s;
-        string ans = "YES";
+int main()
+{
+	int N;
+	cin >> N;
+	
+	for (int i = 0; i < N; i++)
+	{
+		string str;
+		cin >> str;
+		
+		stack<char> s;
 
-        for (int j = 0; j < s.length(); j++) {
-           if (s[j] == '(') {
-               st.push(s[j]);
-           }
-            else if (s[j] == ')' && !st.empty() && st.top() == '(') {
-                st.pop();
-            }
-            else {
-                ans = "NO";
-                break;
-            }
-        }
-        if (!st.empty()) ans = "NO";
-        cout << ans << endl;
-    }
-    return 0;
+		string ans = "YES";
+
+		for (int j = 0; j < str.length(); j++)
+		{
+			// 조건세우기
+			if (str[j] == '(')
+			{
+				s.push('(');
+			}
+			else if (str[j] == ')' && !s.empty() && s.top() == '(')
+			{
+				s.pop();
+			}
+			else
+			{
+				ans = "NO";
+				break;
+			}
+		}
+		if (!s.empty()) ans = "NO";
+		cout << ans << '\n';
+	}
 }
